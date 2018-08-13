5ad2421f1b025a2e8e96d77374a3dc2379b1ca2b
Ajit Nath <nath.ajit01@gmail.com>
2018-08-13 23:50:11 +0800

    defining data types

--- src/HashMap.c
+++ src/HashMap.c
@@ -7,8 +7,30 @@ typedef void *any_t;
 
 
 /* Define a bucket */
+
+/*
+  key: Hashing key
+  data: Value associated with the key
+  in_use: Whether the bucket is in use or not
+*/
+
 typedef struct _hashmap_elem{
   int key;
   any_t data;
   int in_use
 } hashmap_elem ;
+
+
+/*Define Hashmap*/
+
+/*
+  max_size: Maximum size of the hash table,
+  size: Current size of the hash table,
+  the data in the hash table
+*/
+
+typedef struct _hashmap_map{
+  int max_size;
+  int size;
+  hashmap_elem *data
+}
